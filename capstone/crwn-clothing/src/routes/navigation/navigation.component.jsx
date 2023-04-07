298276a8ed4f2fd44d861092b1e0c81de8ecd42f
SD1O1 <siddhakar100@gmail.com>
2023-04-07 22:47:19 +0530

    com

--- capstone/crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ capstone/crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,4 @@
 import { Outlet,Link } from "react-router-dom";
-
 import { Fragment,useContext } from "react";
 import { ReactComponent as CrwnLogo} from "../../assets/crown.svg";
 
@@ -7,6 +6,7 @@ import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import './navigation.styles.scss';
+
 import {UserContext} from '../../contexts/user.context';
 import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
