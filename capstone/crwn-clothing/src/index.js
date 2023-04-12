cd402c32bdba741a5c36943ee3fd4c4e0c6792f2
SD1O1 <siddhakar100@gmail.com>
2023-04-12 15:24:05 +0530

    persist_

--- capstone/crwn-clothing/src/index.js
+++ capstone/crwn-clothing/src/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
      {/* <UserProvider> */}
       {/* <CategoriesProvider> */}
@@ -23,6 +25,7 @@ root.render(
       {/* </CategoriesProvider> */}
      {/* </UserProvider> */}
     </BrowserRouter>
+    </PersistGate>
     </Provider>
   </React.StrictMode>
 );
